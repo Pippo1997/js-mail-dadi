71e6ffe1d3ef1d39f197e334ad06ca320fbc9e8c
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-11-30 16:09:45 +0100

    debug

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -2,31 +2,29 @@
 
 // 1- Genera un numero casuale per il giocatore
 
-numero_giocatore = Math.floor(Math.random() * 5) +1;
+numero_giocatore = Math.floor(Math.random() * 6) +1;
 
 // 2- Genera un numero casuale per il computer
 
-numero_computer = Math.floor(Math.random() * 5) +1;
+numero_computer = Math.floor(Math.random() * 6) +1;
 
 // 3- Controllare il valore più alto
 //     3.1-SE il giocatore ha il numero più alto vince e mostrare in console.log
 //     3.2-SE il computer ha il numero più alto vince e mostrare in console.log
 //     3.3-ALTRIMENTI se il giocatore e il computer hanno lo stesso numero è un pareggio e mostrare in console.log
 
+console.log(`il numero del giocatore è ${numero_giocatore}`)
+console.log(`il numero del computer è ${numero_computer}`)
+
+
 if(numero_giocatore > numero_computer){
     console.log(`il giocatore ha vinto`)
-    console.log(numero_giocatore)
-    console.log(numero_computer)
 }
 
 else if(numero_computer > numero_giocatore){
     console.log(`il computer ha vinto`)
-    console.log(numero_giocatore)
-    console.log(numero_computer)
 }
 
 else{
     console.log(`pareggio`)
-    console.log(numero_giocatore)
-    console.log(numero_computer)
 }
