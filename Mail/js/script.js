1e20949e0377f45589d4ea446b03d1be01a502a6
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-11-30 14:57:29 +0100

    readme mail

---
+++ Mail/js/script.js
@@ -0,0 +1,8 @@
+// PROBLEMA:Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.  Non utilizzare le funzioni indexOf o includes!
+
+// 1-Crea una lista contenente le email di chi puo accedere al sito
+// 2-Permettere l'utente di inserire la propria mail con un prompt
+// 3-tramite il for cerca nella lista delle email se quella che ha inserito l'utente sia presente
+//     3.1-SE è presente mostro in cosole.log che è presente
+//     3.2-ALTRIMENTI mostro in cosole.log che non è presente
+
